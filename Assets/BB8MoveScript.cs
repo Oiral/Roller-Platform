2250c4cb95d433ff3143ec3ad4aaf2885e4b522a
Unknown <hamish@pageland.co.nz>
2018-08-06 08:47:43 +1200

    Added in a jump + downgrade to unity version 2018.1.4

--- Assets/BB8MoveScript.cs
+++ Assets/BB8MoveScript.cs
@@ -11,6 +11,8 @@ public class BB8MoveScript : MonoBehaviour {
     public float vertRotSpeed = 2;
     public float horiRotSpeed = 1;
 
+    public float jumpForce = 1;
+
     private void Start()
     {
         rb = bb8Base.GetComponent<Rigidbody>();
@@ -34,6 +36,10 @@ public class BB8MoveScript : MonoBehaviour {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         mouseX = Input.GetAxisRaw("Mouse X");
         mouseY = Input.GetAxisRaw("Mouse Y");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
@@ -62,5 +68,20 @@ public class BB8MoveScript : MonoBehaviour {
         transform.Rotate(new Vector3(-mouseY * vertRotSpeed, 0));
         transform.Rotate(Vector3.up, mouseX * horiRotSpeed, Space.World);
         //transform.Rotate(transform.right, -mouseY);
+    }
+
+    void Jump()
+    {
+        //Test to see if there is a hit using a BoxCast
+        //Calculate using the center of the GameObject's Collider(could also just use the GameObject's position), half the GameObject's size, the direction, the GameObject's rotation, and the maximum distance as variables.
+        //Also fetch the hit data
+        RaycastHit hit;
+        if (Physics.BoxCast(transform.position, new Vector3(0.2f, 0.2f, 0.2f), -Vector3.up, out hit, Quaternion.identity, 0.5f))
+        {
+            //Output the name of the Collider your Box hit
+            Debug.Log("Hit : " + hit.collider.name);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+        
     }
 }
