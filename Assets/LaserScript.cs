03bd2b2403f50325d9c679ccd0eda367f8cd2974
Unknown <hamish@pageland.co.nz>
2018-08-06 13:55:27 +1200

    Basic Rope Functionality

--- Assets/LaserScript.cs
+++ Assets/LaserScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class LaserScript : MonoBehaviour {
 
     public GameObject laserPoint;
+    public GameObject ropePrefab;
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire2"))
         {
             RaycastHit hit;
 
@@ -25,5 +26,28 @@ public class LaserScript : MonoBehaviour {
         }
         else
             laserPoint.SetActive(false);
-	}
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position + transform.up, transform.forward, out hit))
+            {
+                GameObject rope = Instantiate(ropePrefab, hit.point, Quaternion.LookRotation(transform.position - hit.point));
+                rope.GetComponent<RopeScript>().dist = (int)((float)hit.distance * (float)5);
+                Debug.Log(hit.distance);
+            }
+        }
+        else if (Input.GetButton("Fire1"))
+        {
+            HingeJoint hinge = gameObject.GetComponent<BB8MoveScript>().bb8Base.AddComponent<HingeJoint>();
+            hinge.connectedBody = GameObject.FindGameObjectWithTag("RopeEnd").GetComponent<Rigidbody>();
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            Destroy(gameObject.GetComponent<BB8MoveScript>().bb8Base.GetComponent<HingeJoint>());
+            Destroy(GameObject.FindGameObjectWithTag("RopeEnd").transform.parent.gameObject);
+        }
+    }
 }
---
+++ Assets/RopeScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour {
+
+    Rigidbody rb;
+
+    public int dist = 1;
+
+    public GameObject ropePart;
+
+    private void Start()
+    {
+        gameObject.AddComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        for (int i = 0; i < dist; i++)
+        {
+            Instantiate(ropePart, Vector3.Lerp(transform.position, GameObject.FindGameObjectWithTag("PlayerBase").transform.position,  (float)i / (float)dist), Quaternion.identity, transform);
+        }
+
+        int childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform t = transform.GetChild(i);
+            t.gameObject.AddComponent<HingeJoint>();
+
+            t.gameObject.GetComponent<Rigidbody>().mass = 0.3f;
+            t.gameObject.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+            HingeJoint hinge = t.gameObject.GetComponent<HingeJoint>();
+
+            hinge.connectedBody = i == 0 ? rb :
+                transform.GetChild(i - 1).GetComponent<Rigidbody>();
+
+            hinge.useSpring = true;
+            if (i == childCount - 1)
+            {
+                t.tag = "RopeEnd";
+            }
+        }
+    }
+}
