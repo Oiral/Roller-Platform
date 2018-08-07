217b2b0d05799a4e9f8ce6475db78d3b1c6f2559
Unknown <hamish@pageland.co.nz>
2018-08-08 08:29:07 +1200

    Changes to the map

--- Assets/LaserScript.cs
+++ Assets/LaserScript.cs
@@ -6,7 +6,9 @@ public class LaserScript : MonoBehaviour {
 
     public GameObject laserPoint;
     public GameObject ropePrefab;
-	
+
+    GameObject rope;
+
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButton("Fire2"))
@@ -34,20 +36,25 @@ public class LaserScript : MonoBehaviour {
 
             if (Physics.Raycast(transform.position + transform.up, transform.forward, out hit))
             {
-                GameObject rope = Instantiate(ropePrefab, hit.point, Quaternion.LookRotation(transform.position - hit.point));
+                rope = Instantiate(ropePrefab, hit.point, Quaternion.LookRotation(transform.position - hit.point));
                 rope.GetComponent<RopeScript>().dist = (int)((float)hit.distance * (float)5);
                 Debug.Log(hit.distance);
             }
         }
-        else if (Input.GetButton("Fire1"))
-        {
-            HingeJoint hinge = gameObject.GetComponent<BB8MoveScript>().bb8Base.AddComponent<HingeJoint>();
-            hinge.connectedBody = GameObject.FindGameObjectWithTag("RopeEnd").GetComponent<Rigidbody>();
-        }
-        else if (Input.GetButtonUp("Fire1"))
-        {
-            Destroy(gameObject.GetComponent<BB8MoveScript>().bb8Base.GetComponent<HingeJoint>());
-            Destroy(GameObject.FindGameObjectWithTag("RopeEnd").transform.parent.gameObject);
+        else {
+            if (rope != null)
+            {
+                if (Input.GetButton("Fire1"))
+                {
+                    HingeJoint hinge = gameObject.GetComponent<BB8MoveScript>().bb8Base.AddComponent<HingeJoint>();
+                    hinge.connectedBody = GameObject.FindGameObjectWithTag("RopeEnd").GetComponent<Rigidbody>();
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    Destroy(gameObject.GetComponent<BB8MoveScript>().bb8Base.GetComponent<HingeJoint>());
+                    Destroy(GameObject.FindGameObjectWithTag("RopeEnd").transform.parent.gameObject);
+                }
+            }
         }
     }
 }
